1d58e683e2586c7303d76a439d38aa082be2369a
joseph <jmcinern@tcd.ie>
2025-09-23 11:58:46 +0100

    just 5 mins

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=QUANT_AWQ_Instruct
 #SBATCH --output=./out/quant_awq_instruct_%j.out
 #SBATCH --error=./err/quant_awq_instruct_%j.err
-#SBATCH --time=00:10:00
+#SBATCH --time=00:05:00
 #SBATCH --partition=k2-gpu-v100  
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks=1
