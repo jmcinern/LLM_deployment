b5ed4047c04dbb8bbbf69972087bedf18d259f3d
joseph <jmcinern@tcd.ie>
2025-09-23 12:33:34 +0100

    trying obscure gpu partition for potential q jump

--- run.sh
+++ run.sh
@@ -3,18 +3,20 @@
 #SBATCH --output=./out/quant_awq_instruct_%j.out
 #SBATCH --error=./err/quant_awq_instruct_%j.err
 #SBATCH --time=00:05:00
-#SBATCH --partition=k2-gpu-v100  
-#SBATCH --gres=gpu:1
+#SBATCH --partition=k2-gpu-a100mig  
+#SBATCH --gres=gpu:3g.30gb:1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=64G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=josephmcinerney7575@gmail.com
-
+'''
 module load python3/3.10.5/gcc-9.3.0 # available python
 module load libs/nvidia-cuda/12.4.0/bin # cuda
 source /mnt/scratch2/users/40460549/cpt-dail/awq_env/bin/activate
-cd $SLURM_SUBMIT_DIR
+pip install -r requirements.txt
+
+'''
 
 # run it
 python quantize_awq.py
