680e957a431aac863c04cb6f8ea1cc3f183a165e
joseph <jmcinern@tcd.ie>
2025-09-23 11:58:24 +0100

    dont redownload requirements each time, no 2 CPU for lower resource request

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=QUANT_AWQ
-#SBATCH --output=./out/quant_awq_%j.out
-#SBATCH --error=./err/quant_awq_%j.err
+#SBATCH --job-name=QUANT_AWQ_Instruct
+#SBATCH --output=./out/quant_awq_instruct_%j.out
+#SBATCH --error=./err/quant_awq_instruct_%j.err
 #SBATCH --time=00:10:00
 #SBATCH --partition=k2-gpu-v100  
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=1
 #SBATCH --mem=64G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=josephmcinerney7575@gmail.com
@@ -14,10 +14,7 @@
 module load python3/3.10.5/gcc-9.3.0 # available python
 module load libs/nvidia-cuda/12.4.0/bin # cuda
 source /mnt/scratch2/users/40460549/cpt-dail/awq_env/bin/activate
-pip install -r "requirements.txt"
 cd $SLURM_SUBMIT_DIR
-
-export HF_TOKEN="hf_your_token_here"
 
 # run it
 python quantize_awq.py
