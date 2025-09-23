5a5763a549a287ce63db9d7213eba2b12a8d16c7
joseph <jmcinern@tcd.ie>
2025-09-23 12:51:41 +0100

    more time for full run

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=QUANT_AWQ_Instruct
 #SBATCH --output=./out/quant_awq_instruct_%j.out
 #SBATCH --error=./err/quant_awq_instruct_%j.err
-#SBATCH --time=00:05:00
+#SBATCH --time=00:45:00
 #SBATCH --partition=k2-gpu-a100  
 #SBATCH --gres=gpu:a100:1
 #SBATCH --ntasks=1
